8cb5ce780d0f6af138458a78e6aabbf88f0c6c0a
young chen <1137986780@qq.com>
2022-05-13 16:37:18 +0800

    chore: support electron-builder

--- electron/main.js
+++ electron/main.js
@@ -1,7 +1,7 @@
 // main.js
 // 控制应用生命周期和创建原生浏览器窗口的模组
 const { app, BrowserWindow, globalShortcut } = require("electron")
-const isDev = require("electron-is-dev")
+// const isDev = require("electron-is-dev")
 const path = require("path")
 
 function createWindow() {
@@ -20,7 +20,6 @@ function createWindow() {
             preload: path.join(__dirname, "preload.js"),
         },
     })
-    win1.loadURL("http://localhost:3000")
 
     const win2 = new BrowserWindow({
         width: 80,
@@ -35,7 +34,6 @@ function createWindow() {
             preload: path.join(__dirname, "preload2.js"),
         },
     })
-    win2.loadURL("http://localhost:3000")
 
     const win3 = new BrowserWindow({
         width: 80,
@@ -50,17 +48,25 @@ function createWindow() {
             preload: path.join(__dirname, "preload3.js"),
         },
     })
-    win3.loadURL("http://localhost:3000")
-    // if (isDev) {
-    //     mainWindow.loadURL("http://localhost:3000")
-    //     // openDevTools
-    //     mainWindow.webContents.openDevTools()
-    // } else {
-    //     mainWindow.loadFile(
-    //         // vue 3's build result
-    //         `file://${path.join(__dirname, "../dist/index.html")}`
-    //     )
-    // }
+    let isDev = false
+    if (isDev) {
+        win1.loadURL("http://localhost:3000")
+        win2.loadURL("http://localhost:3000")
+        win3.loadURL("http://localhost:3000")
+        // openDevTools
+        // mainWindow.webContents.openDevTools()
+    } else {
+        // win1.loadFile(
+        //     // vue 3's build result
+        //     `file://${path.join(__dirname, "../dist/index.html")}`
+        // )
+        // win2.loadFile(`file://${path.join(__dirname, "../dist/index.html")}`)
+        // win3.loadFile(`file://${path.join(__dirname, "../dist/index.html")}`)
+        win1.loadFile("./dist/index.html")
+        win2.loadFile("./dist/index.html")
+        win3.loadFile("./dist/index.html")
+    }
+    
 }
 
 // 这段程序将会在 Electron 结束初始化
